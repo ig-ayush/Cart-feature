37fb1f4af19932c803c4d025cf72ff2333b70541
Ayush-alt-sys <igayushacharya@gmail.com>
2025-04-17 22:03:34 +0530

    Structre of container

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 const btn = document.querySelectorAll("button");
 
-btn.forEach((element,index) => {
-    btn[index].addEventListener("click",(event)=>{
-        event.preventDefault();
-        let p = `price-${index}`;
-        let price = document.getElementById(`${p}`);
-        console.log(price.textContent);
-    });
+btn.forEach((element, index) => {
+  btn[index].addEventListener("click", (event) => {
+    event.preventDefault();
+    let p = `price-${index}`;
+    let price = document.getElementById(`${p}`);
+    console.log(price.textContent);
+  });
 });
