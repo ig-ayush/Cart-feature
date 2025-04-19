c6dec8dd65a8650bde2ca1da50bdc288b114d297
Ayush-alt-sys <igayushacharya@gmail.com>
2025-04-19 23:28:58 +0530

    finish the css part

--- src/script.js
+++ src/script.js
@@ -4,7 +4,14 @@ btn.forEach((element, index) => {
   btn[index].addEventListener("click", (event) => {
     event.preventDefault();
     let p = `price-${index}`;
+    let pr_name = `pr-${index}`;
     let price = document.getElementById(`${p}`);
-    console.log(price.textContent);
+
+    let product_name = document.getElementById(pr_name).textContent ;
+    let product_price = price.textContent;
+
+    console.log(product_name, "",product_price)
+
+    
   });
 });
